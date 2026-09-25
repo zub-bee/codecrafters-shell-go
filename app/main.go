b2ccf52af5a3b873890e624fcdb39b635f57b13b
zub-bee <zubbeed21.18@gmail.com>
2026-09-25 13:21:27 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	if command != "" {
+		str := strings.Split(command, "\n")
+		fmt.Printf("%s: command not found\n", str[0])
+	}
 }
