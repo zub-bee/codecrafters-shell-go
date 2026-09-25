8c2e7f59ebe11e681c2feac4a5831f0d45af1883
zub-bee <zubbeed21.18@gmail.com>
2026-09-25 13:36:24 +0100

    feat: print error message for unknown commands & make repl [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,9 +7,12 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
-	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	if command != "" {
-		fmt.Printf("%s: command not found\n", command[:len(command)-1])
+	for true {
+		fmt.Print("$ ")
+		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		if command != "" {
+			fmt.Printf("%s: command not found\n", command[:len(command)-1])
+		}
+
 	}
 }
